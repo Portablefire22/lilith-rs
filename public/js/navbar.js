c8c858df9a482b6d061f11ba8c1c4e512ab48407
Portablefire22 <barnowlsarecool@gmail.com>
2025-07-01 19:54:48 +0100

    Added cookies for navbar tracking per session Prevents navbar from changing everytime a link is clicked

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -8,6 +8,7 @@ function navbarLeft() {
   if (index < 0) {
     index = navbarItems.length - 1;
   }
+  setCookie("navbar", index);
   navbarItems[index].id = "navbar-active";
 }
 
@@ -17,5 +18,20 @@ function navbarRight() {
   if (index > navbarItems.length - 1) {
     index = 0;
   }
+  console.log(index);
+  setCookie("navbar", index);
   navbarItems[index].id = "navbar-active";
 }
+
+
+function loadNavbar() {
+  let cookie = getCookie("navbar");
+  if (cookie === undefined) {
+    setCookie("navbar", navbarItems.length - 1);
+    cookie = navbarItems.length - 1;
+  }
+  index = cookie;
+  navbarItems[index].id = "navbar-active";
+}
+
+loadNavbar();
--- public/js/theme.js
+++ public/js/theme.js
@@ -11,10 +11,21 @@ function getCookie(name) {
   }
 }
 
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value)  + expires + "; path=/";
+}
+
 function loadTheme() {
   console.log(document.cookie);
   let cookie = getCookie("theme");
   if (cookie === undefined) {
+    setCookie("theme", "dark", 30);
     cookie = "dark";
   }
   setTheme(cookie);
